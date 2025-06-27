4a6edcbba5c20279979839b0176a207b9ae8d353
Alex <alreva@gmail.com>
2025-06-27 16:54:46 +0300

    fix: address top style warnings

---
+++ Dto/SocialMedia.cs
@@ -0,0 +1,17 @@
+// <copyright file="SocialMedia.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Dto;
+
+/// <summary>
+/// Social media links.
+/// </summary>
+public class SocialMedia
+{
+  public required string Facebook { get; set; }
+
+  public required string Twitter { get; set; }
+
+  public required string Instagram { get; set; }
+}
