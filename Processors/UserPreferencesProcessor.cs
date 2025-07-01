5d0e89205077bcb2ec6bb0126154c6698f597024
Alex <alreva@gmail.com>
2025-07-01 14:27:20 +0300

    style: apply automatic formatting

--- Processors/UserPreferencesProcessor.cs
+++ Processors/UserPreferencesProcessor.cs
@@ -8,6 +8,7 @@ using Dto;
 
 public class UserPreferencesProcessor : BaseProcessor<UserEventDto>
 {
+  /// <inheritdoc/>
   public override void Process(string jsonInput, TextWriter output)
   {
     var userEventDto = this.Deserialize(jsonInput);
